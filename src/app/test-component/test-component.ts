9e0067d3890190fa8a3337885d3d9dfa2952fa09
Datn7 <datunashvili72@gmail.com>
2025-06-14 03:51:56 +0400

    updated and fixed api call

--- src/app/test-component/test-component.ts
+++ src/app/test-component/test-component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { TestService } from '../test.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test',
   standalone: true,
+  imports:[CommonModule],
   templateUrl: './test-component.html'
 })
 export class TestComponent implements OnInit {
-  message = '';
+ 
 
   constructor(private testService: TestService) {}
 
-  ngOnInit() {
-    console.log('TestComponent initialized'); // 👈 Add this
+  message = '';
 
-    this.testService.getMessage().subscribe({
-      next: res => {
-        console.log('API Response:', res); // 👈 Add this
-        this.message = res.message;
-      },
-      error: err => {
-        console.error('API Error:', err);
-        this.message = 'Error: ' + err.message;
-      }
-    });
-  }
+ngOnInit() {
+  this.testService.getMessage().subscribe({
+    next: res => {
+      console.log('API Response:', res);
+      this.message = res.message; // ✅ Must assign this!
+    },
+    error: err => {
+      console.error('API Error:', err);
+      this.message = 'Error: ' + err.message;
+    }
+  });
+}
+
 }
