8d7574e88dcc6c96a93fcd063f027ffda2ffd142
Datn7 <datunashvili72@gmail.com>
2025-06-16 19:48:27 +0400

    added delete button

--- src/app/memories/memories.component.ts
+++ src/app/memories/memories.component.ts
@@ -3,13 +3,12 @@ import { MemoryService } from '../services/memory.service';
 import { Memory } from '../models/memory.model';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
-    selector: 'app-memories',
+  selector: 'app-memories',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './memories.component.html',
-  styleUrls: ['./memories.component.scss']
+  styleUrls: ['./memories.component.scss'],
 })
 export class MemoriesComponent implements OnInit {
   memories: Memory[] = [];
@@ -18,8 +17,16 @@ export class MemoriesComponent implements OnInit {
 
   ngOnInit(): void {
     this.memoryService.getAll().subscribe({
-      next: (data) => this.memories = data,
-      error: (err) => console.error('Failed to load memories:', err)
+      next: (data) => (this.memories = data),
+      error: (err) => console.error('Failed to load memories:', err),
     });
+  }
+
+  deleteMemory(id: number): void {
+    if (confirm('Are you sure you want to delete this memory?')) {
+      this.memoryService.deleteMemory(id).subscribe(() => {
+        this.memories = this.memories.filter((m) => m.id !== id);
+      });
+    }
   }
 }
--- src/app/services/memory.service.ts
+++ src/app/services/memory.service.ts
@@ -5,21 +5,22 @@ import { Memory } from '../models/memory.model';
 import { environment } from '../../environments/environment.server';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MemoryService {
-  
-  private apiUrl = `${environment.apiBaseUrl}/memories`;
+  private apiUrl = `${environment.apiBaseUrl}/`;
 
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Memory[]> {
-    return this.http.get<Memory[]>(this.apiUrl);
+    return this.http.get<Memory[]>(this.apiUrl + 'memories');
   }
 
-  
+  addMemory(memory: Memory): Observable<any> {
+    return this.http.post(`${environment.apiBaseUrl}/memories/`, memory);
+  }
 
- addMemory(memory: Memory): Observable<any> {
-  return this.http.post(`${environment.apiBaseUrl}/memories/`, memory);
-}
+  deleteMemory(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}memories/${id}`);
+  }
 }
